b70123e3d9592ae205a90b7333c89558f0eb8370
Mehmet <Mehmet@MEHMETDUZDABAN>
2021-05-10 10:51:08 +0300

    24-25-26

---
+++ Intro2/src/Main.java
@@ -0,0 +1,81 @@
+
+public class Main {
+
+	/* DERS 24 
+	public static void main(String[] args) {
+		SayýBulmaca();
+	}
+	
+	public static void SayýBulmaca () {
+		int[] sayilar = new int[] {1,2,3,4,5,6,7,8,9,10};
+		int aranacak = 1;
+		boolean varMi = false;
+
+		for( int sayi :sayilar) {
+			if(sayi==aranacak) {
+				varMi=true;
+				break;
+			}
+		}
+		
+		if (varMi) {
+			
+			OlumluMesajVer(aranacak);
+		
+		}else {
+			OlumsuzMesajVer(aranacak);
+		}
+	
+	}
+		
+	public static void OlumluMesajVer (int aranacak) {
+		System.out.println ("Sayý Listede Mevcut :"+aranacak);	
+	}
+	
+	public static void OlumsuzMesajVer (int aranacak) {
+		System.out.println ("Sayý Listede Mevcut Deðil:"+aranacak);	
+	}
+	*/
+	
+	//------------------------------------------------------------------//
+	/*
+	// DERS 25-26
+	public static void main(String[] args) {
+		String yeniMesaj = sehirver();
+		System.out.println (yeniMesaj);
+		//Variable Arguments ile Çalýþmak
+		int toplam = topla(2,3,4,5,7,8);
+		System.out.println (toplam);
+	}
+	
+	public static void ekle() {
+		System.out.println ("Sayý Eklendi");
+	}
+	
+	public static void sil() {
+		System.out.println ("Sayý Silindi");
+	}
+	
+	public static void güncelle() {
+		System.out.println ("Sayý Güncellendi");
+	}
+
+	public static String sehirver() {
+		return "Ankara";
+	}
+	
+	//Variable Arguments ile Çalýþmak
+	public static int topla(int... sayilar) {
+		int toplam=0;
+		for (int sayi:sayilar) {
+			toplam+=sayi;
+		}
+		return toplam;
+	}	
+	*/
+
+	
+	
+	
+	
+}
